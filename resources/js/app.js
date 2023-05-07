5db74bc0068bacc67ad9ed08a3d15797f661f691
ludoguenet <ludovicguenet@gmx.com>
2023-05-07 15:16:59 +0200

    added vue and vue-plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from "vue";
+import UsersList from "./components/users/UsersList.vue";
+
+const app = createApp({});
+
+app.component('usersList', UsersList);
+
+app.mount('#app');
