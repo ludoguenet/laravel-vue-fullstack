fceed49e0a263dae34cf1201a4f3ec1c0f0abae0
ludoguenet <ludovicguenet@gmx.com>
2023-05-07 15:54:20 +0200

    added UsersList component

--- resources/js/components/users/UsersList.vue
+++ resources/js/components/users/UsersList.vue
@@ -1,7 +1,23 @@
 <template>
-    <h1>Hello World</h1>
+    <template
+        v-for="user in users"
+    >
+        <p
+            v-text="user.name"
+        />
+    </template>
 </template>
 
 <script setup>
+    import {ref} from "vue";
 
+    const users = ref([]);
+
+    const loadFromServer = () => {
+        axios.get('/api/users')
+            .then((res) => users.value = res.data.data)
+            .catch((err) => console.log(err));
+    }
+
+    loadFromServer();
 </script>
